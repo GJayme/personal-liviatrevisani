6563a421af1ea2c90fbeea7a5e2de04daeac7bb1
Gabriel Jayme <gabrielj_89@hotmail.com>
2022-08-02 11:42:52 -0300

    PersonalLiviatrevisaniApplicationTests file.

---
+++ src/test/java/br/com/personalliviatrevisani/personalliviatrevisani/PersonalLiviatrevisaniApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.personalliviatrevisani.personalliviatrevisani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalLiviatrevisaniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
