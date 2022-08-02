75b34a8bc4dd7b0ff8be603bece983779b054da7
Gabriel Jayme <gabrielj_89@hotmail.com>
2022-08-02 11:41:04 -0300

    WIP AnamnesisModel.

---
+++ src/main/java/br/com/personal/livia/model/AnamnesisModel.java
@@ -0,0 +1,18 @@
+package br.com.personal.livia.model;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+public class AnamnesisModel {
+
+    //todo confirmar com a Lih o modelo do questionario com as perguntas e respostas
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private UUID id;
+//
+//    @OneToOne(cascade = CascadeType.ALL)
+//    @JoinColumn(name = "CLIENT_ID", referencedColumnName = "id")
+//    private ClientModel clientModel;
+
+
+}
