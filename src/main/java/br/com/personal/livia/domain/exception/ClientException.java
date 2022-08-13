2b8fec0e64e67971ccdac49ba56b87022ab8d6af
Gabriel Jayme <gabrielj_89@hotmail.com>
2022-08-13 12:53:04 -0300

    Feat: Add message in client exception.

--- src/main/java/br/com/personal/livia/domain/exception/ClientException.java
+++ src/main/java/br/com/personal/livia/domain/exception/ClientException.java
@@ -2,6 +2,6 @@ package br.com.personal.livia.domain.exception;
 
 public class ClientException extends CustomException {
     public ClientException(String message) {
-        super(message);
+        super("Client error: " + message);
     }
 }
